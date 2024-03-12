32ae6cd0d9e816582ba0ced953646a7cef529338
Fábio cabral <fabio.brito.cabral@gmail.com>
2024-03-12 15:26:58 +0000

    #implementação da recuperação e password , e testes funcionais

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
-""" path('accounts/', include("django.contrib.auth.urls"))  """
-   
 urlpatterns = [
-    path("accounts/register/",views.register, name="register"),
-    path('accounts/login/', views.login_view, name='login')
-    
-
-    
+    path("accounts/register/", views.register, name="register"),
+    path("accounts/login/", views.login_view, name="login"),
+    path("accounts/password_reset/", auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset"),
+    path("accounts/reset_password_sent/", auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"), name="password_reset_done"),
+    path("accounts/reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name="password_reset_confirm"),
+    path("accounts/reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name="password_reset_complete"),
 ]
