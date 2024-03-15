37f268ed1dd5c12b6272f6c5ae91458f58c8b1e5
Fábio cabral <fabio.brito.cabral@gmail.com>
2024-03-15 12:54:23 +0000

    #testes recuperação password

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("accounts/password_reset/", auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset"),
     path("accounts/reset_password_sent/", auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"), name="password_reset_done"),
     path("accounts/reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_confirm.html"), name="password_reset_confirm"),
-    path("accounts/reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name="password_reset_complete"),
+    path("accounts/reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_complete.html"), name="password_reset_complete"),
 ]
