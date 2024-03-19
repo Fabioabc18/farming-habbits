6f6e54ff4a5bf85236a1ea4b012bac4c8f9cc261
Fábio cabral <fabio.brito.cabral@gmail.com>
2024-03-19 16:59:27 +0000

    #implementação das "quotes" na homepage através de uma api externa

--- home/views.py
+++ home/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
+import requests
 
-# Create your views here.
 def home(request):
-    template = loader.get_template("home.html")
-    
+    url = "https://quotes15.p.rapidapi.com/quotes/random/"
+    querystring = {"language_code": "pt"}
 
-    return HttpResponse(template.render(request=request))
+    headers = {
+        "X-RapidAPI-Key": "c7dbb48416mshc7b606bd9ea37d5p142e70jsn559215ad5281",
+        "X-RapidAPI-Host": "quotes15.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    quote_data = response.json()
+
+    template = loader.get_template("home.html")
+    context = {
+        'quote_data': quote_data
+    }
+    return HttpResponse(template.render(context, request))
